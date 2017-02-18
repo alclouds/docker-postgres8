5000e77a70aee47c9e200dd99eab33cc3e00eadb
Kim Wu <kimwu@live.hk>
2017-02-18 16:51:51 +0800

    correct text

--- 8.1/start.sh
+++ 8.1/start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-/etc/init.d/
 /etc/init.d/postgresql start
 
-pid=`pidof -s "/usr/bin/postmaster"`
+pid=`pidof -s /usr/bin/postmaster`
 if [ $pid ] && [ -f "/var/lib/pgsql/data/postmaster.pid" ]
 then
   cat /var/lib/pgsql/pgstartup.log
   echo '---------------------------------------------------'
   tail -f /var/lib/pgsql/pgstartup.log
-fi 
+fi
